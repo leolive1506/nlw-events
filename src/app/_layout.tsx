0e528a88a0cb1262c16533e16eddf6049d7f2492
Leonardo Lopes <leonardo.santana@gmail.com>
2024-04-05 23:43:48 -0300

    install native wind

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Slot } from 'expo-router'
+import '../styles/global.css'
+import { StatusBar } from 'expo-status-bar'
+
+export default function Layout() {
+  return (
+    <>
+      <StatusBar style='light'/>
+      <Slot />
+    </>
+  )
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native'
 
 export default function Home() {
     return (
-      <View>
-        <Text>Hello react native</Text>
+      <View className='flex-1 bg-green-500 items-center justify-center text-white'>
+        <Text className='text-white'>Hello react native</Text>
       </View>
     )
 }
