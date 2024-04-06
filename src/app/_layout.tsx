6fb748b8d491828510045ecbe03f013f49a691eb
Leonardo Lopes <leonardo.santana@gmail.com>
2024-04-05 23:51:55 -0300

    install robo fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,19 @@
 import { Slot } from 'expo-router'
 import '../styles/global.css'
 import { StatusBar } from 'expo-status-bar'
+import { useFonts, Roboto_700Bold, Roboto_500Medium, Roboto_400Regular } from '@expo-google-fonts/roboto'
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular
+  })
+
+  if (!fontsLoaded) {
+    return
+  }
+  
   return (
     <>
       <StatusBar style='light'/>
