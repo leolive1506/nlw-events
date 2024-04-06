2e336efae8dd0f7835b2ed296ad677ec466ed148
Leonardo Lopes <leonardo.santana@gmail.com>
2024-04-05 23:59:25 -0300

    create loading component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,10 @@
+import '@/styles/global.css'
+
 import { Slot } from 'expo-router'
-import '../styles/global.css'
 import { StatusBar } from 'expo-status-bar'
 import { useFonts, Roboto_700Bold, Roboto_500Medium, Roboto_400Regular } from '@expo-google-fonts/roboto'
+
+import { Loading } from '@/components/loading'
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
@@ -10,14 +13,10 @@ export default function Layout() {
     Roboto_400Regular
   })
 
-  if (!fontsLoaded) {
-    return
-  }
-  
   return (
     <>
       <StatusBar style='light'/>
-      <Slot />
+      { fontsLoaded ? <Slot /> : <Loading /> }
     </>
   )
 }
